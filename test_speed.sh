3d86d4df46c41a1a396db84a7f17032a8b276069
Jhon Alexander Holguin Barrera <jhon.holguin@aurea.com>
2022-11-25 19:21:42 -0500

    Adding script for speed tests

---
+++ test_speed.sh
@@ -0,0 +1,10 @@
+#! /usr/local/bin/bash
+
+URL='http://localhost:8000/suggest?prefix=a&limit=100'
+
+echo "Request output:"
+curl -X GET "${URL}"
+
+printf "\n============\n"
+echo "Metrics result:"
+curl -w "@curl_format.txt" -o /dev/null -s -X GET "${URL}"  -H "accept: application/json"
